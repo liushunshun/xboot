efc69dd4d530906c8a5e3a6aae9a8c8dba76dfa0
xiuyang <jlss2011@163.com>
2016-10-26 23:30:03 +0800

    thymeleaf

---
+++ src/main/java/com/xy/config/ThymeleafProperties.java
@@ -0,0 +1,32 @@
+/*
+package com.xy.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.MimeType;
+
+import java.nio.charset.Charset;
+
+*/
+/**
+ * Created by Alex on 2016/10/26.
+ *//*
+
+@ConfigurationProperties("spring.thymeleaf")
+public class ThymeleafProperties {
+    private static final Charset DEFAULT_ENCODING = Charset.forName("UTF-8");
+    private static final MimeType DEFAULT_CONTENT_TYPE = MimeType.valueOf("text/html");
+    public static final String DEFAULT_PREFIX = "classpath:/templates/";
+    public static final String DEFAULT_SUFFIX = ".html";
+    private boolean checkTemplateLocation = true;
+    private String prefix = "classpath:/templates/";
+    private String suffix = ".html";
+    private String mode = "HTML5";
+    private Charset encoding;
+    private MimeType contentType;
+    private boolean cache=false;
+    private Integer templateResolverOrder;
+    private String[] viewNames;
+    private String[] excludedViewNames;
+    private boolean enabled;
+}
+*/
--- src/main/java/com/xy/controller/HomeController.java
+++ src/main/java/com/xy/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.xy.controller;
 
+import com.xy.bean.Person;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Alex on 2016/10/25.
@@ -12,5 +18,22 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "hello world";
+    }
+
+    @RequestMapping("/index")
+    public ModelAndView index(Map<String,Object> model){
+        Person single = new Person("kevin", 28);
+
+        List<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("xx", 11);
+        Person p2 = new Person("yy", 22);
+        Person p3 = new Person("zz", 33);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+
+        model.put("singlePerson", single);
+        model.put("people", people);
+        return new ModelAndView("index",model);
     }
 }
